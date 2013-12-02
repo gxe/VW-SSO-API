7cba00cefb05699588add627679cc3f49500dbd2
Nick Wilging <nickwilg@gmail.com>
2013-12-01 21:00:58 -0800

    Initial Commit

--- includes/SSOConnector.php
+++ includes/SSOConnector.php
@@ -10,6 +10,8 @@
 define(MAX_BUFFER_SIZE, 2048); # Set buffer to 2048 byte maximum
 
 class SSO {
+
+
 	/*
 	 * To instantiate, user must provide their SSO username and password, which will act as the
 	 * operator account for the duration of the connection.
